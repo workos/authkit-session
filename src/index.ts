0486600791d404604aca1e4efa15e309564f1f9d
Nick Nisi <nick.nisi@workos.com>
2025-04-23 06:57:43 -0500

    update build

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { getWorkOS } from './core/workos';
 export { configure, getConfig } from './core/config';
 export { SessionManager } from './core/session/SessionManager';
 export { CookieSessionStorage } from './core/session/CookieSessionStorage';
+export { createAuthKit } from './core/factory';
 export * from './core/session/types';
 export * from './core/config/types';
