7eb307b7e1118e06a78a301a0f071483199d7323
Nick Nisi <nick.nisi@workos.com>
2025-09-10 18:55:33 -0500

    add utils tests

---
+++ src/utils.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from 'vitest';
+import { once } from './utils';
+
+describe('utils', () => {
+  describe('once', () => {
+    it('only allows the provided function to be called once, caching its result', () => {
+      let num = 0;
+      const fn = once(() => ++num);
+      expect(fn()).toEqual(1);
+      expect(fn()).toEqual(1); // test it's the same on every call
+    });
+  });
+});
